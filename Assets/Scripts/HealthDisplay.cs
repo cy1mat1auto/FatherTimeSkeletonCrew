7abdcf7a7f7d20c24f6ba89e96e8822a4862af2b
cy1mat1auto <46876701+cy1mat1auto@users.noreply.github.com>
2019-01-26 21:45:06 -0500

    Health Bar
    The health bar (and by extension the player health) is fully scripted. Appearance of the UI elements will be discussed at next week's meeting.

---
+++ Assets/Scripts/HealthDisplay.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplay : MonoBehaviour
+{
+
+    public GameObject Jockey;
+    public Text HealthNumber;
+    public float MaxHealth;
+    public float CurrentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentHealth = Jockey.GetComponent<PlayerHealth>().CurrentHealth;
+        MaxHealth = Jockey.GetComponent<PlayerHealth>().MaxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CurrentHealth = Jockey.GetComponent<PlayerHealth>().CurrentHealth;
+        GetComponent<Slider>().value = CurrentHealth / MaxHealth;
+        HealthNumber.text = CurrentHealth.ToString();
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public float MaxHealth;
+    public float CurrentHealth;
+    public float HealthBounds;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HealthBounds = Mathf.Clamp(CurrentHealth, 0, MaxHealth);
+        CurrentHealth = HealthBounds;
+        CurrentHealth = MaxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
