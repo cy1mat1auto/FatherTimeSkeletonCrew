df5a45fb7536a9eb0b8f4f2c709631294948265a
cy1mat1auto <46876701+cy1mat1auto@users.noreply.github.com>
2019-01-26 21:53:03 -0500

    Update PlayerHealth.cs
    Mathf.Clamp was on the wrong line (Derp). Health is working now.

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -7,20 +7,17 @@ public class PlayerHealth : MonoBehaviour
 
     public float MaxHealth;
     public float CurrentHealth;
-    public float HealthBounds;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        HealthBounds = Mathf.Clamp(CurrentHealth, 0, MaxHealth);
-        CurrentHealth = HealthBounds;
         CurrentHealth = MaxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CurrentHealth = Mathf.Clamp(CurrentHealth, 0, MaxHealth);
     }
 }
