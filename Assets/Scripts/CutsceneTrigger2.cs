cb6edb9a37be4f7174ec9fe988c57d9bbfdb2247
cy1mat1auto <46876701+cy1mat1auto@users.noreply.github.com>
2019-01-31 08:51:59 -0500

    CutscenTrigger2.1
    CutsceneTrigger2 is now more universal than ever before. I want to add another list for individual scene timing down the line.

--- Assets/Scripts/CutsceneTrigger2.cs
+++ Assets/Scripts/CutsceneTrigger2.cs
@@ -16,10 +16,12 @@ public class CutsceneTrigger2 : MonoBehaviour
     //An index to keep track of cutscene progression
     public int SceneProgress;
     //A bunch of UI Sprites; drag-and-drop is probably the best way to deal with this
-    public Sprite im1;
-    public Sprite im2;
-    public Sprite im3;
+    public Sprite im1; public Sprite im2; public Sprite im3; public Sprite im4; public Sprite im5;
+    public Sprite im6; public Sprite im7; public Sprite im8; public Sprite im9; public Sprite im10;
+    //A value, in case your cutscene does not fill this whole script. This should default to the maximum length of the script;
+    public int SceneLength = 10;
     //This part allows us to create a list of the UI sprites used to generate the cutscene, allowing us to iterate through the scenes in order
+    public List<Sprite> BufferStack;
     public List<Sprite> SceneStack;
 
     private void OnTriggerEnter(Collider other)
@@ -27,9 +29,10 @@ public class CutsceneTrigger2 : MonoBehaviour
 
         if (other.CompareTag("Player"))
         {
-            Sprite[] input = { im1, im2, im3 };
-            SceneStack.AddRange(input);
-            Debug.Log("Player entered trigger");
+            Sprite[] input = { im1, im2, im3, im4, im5, im6, im7, im8, im9, im10 };
+            BufferStack.AddRange(input);
+            SceneStack.AddRange(BufferStack.GetRange(0, SceneLength));
+            Debug.Log(SceneStack.Count);
             Time.timeScale = 0;
             timing = Time.unscaledTime;
 
@@ -54,6 +57,11 @@ public class CutsceneTrigger2 : MonoBehaviour
             timing = Time.unscaledTime;
             SceneProgress += 1;
             cutScene.GetComponent<Image>().sprite = SceneStack[SceneProgress];
+        }
+
+        else if (Time.unscaledTime - timing >= Duration && SceneProgress == SceneStack.Count -1)
+        {
+            cutSceneDone = true;
         }
 
         // exits cutscene if player presses correct exit key(s)
