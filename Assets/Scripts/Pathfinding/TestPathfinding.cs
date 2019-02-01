28b1b97cebf1fd33d0262862d159e0d2267a9e97
zSean <b12205@hotmail.com>
2019-02-01 16:04:32 -0500

    Basic pathfinding

---
+++ Assets/Scripts/Pathfinding/TestPathfinding.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPathfinding : MonoBehaviour
+{
+    GameObject goal;
+    List<Waypoint> followPath = new List<Waypoint>();
+
+    WaypointManager manager;    // Turn this static once things are figured out
+
+    float distanceC = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        goal = GameObject.FindWithTag("Finish");
+
+        manager = gameObject.GetComponent<WaypointManager>();
+        manager.Init(); // TEMPORARY. MAKE WAYPOINT MANAGER STATIC AND REMOVE THIS LATER!!!
+
+        followPath = manager.FindPath(manager.FindClosestWaypoint(transform), goal.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (followPath.Count > 0)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, followPath[0].gameObject.transform.position, 10);
+            distanceC += 10;
+
+            if (followPath[0].transform.position == transform.position)
+                followPath.RemoveAt(0);
+        }
+        else
+            print(distanceC);
+    }
+}
---
+++ Assets/Scripts/Pathfinding/WaypointManager.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Holds information about all the waypoints, as well as initial setup.
+public class WaypointManager : MonoBehaviour
+{
+    /*static*/ List<Waypoint> waypoints = new List<Waypoint>(); // Keeps track of all the waypoints
+
+    // Start is called before the first frame update
+    public void Init()
+    {
+        GameObject[] collectWaypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+
+        for (int i = 0; i < collectWaypoints.Length; i++)
+            waypoints.Add(collectWaypoints[i].GetComponent<Waypoint>());
+    }
+
+    // Use a Kd-Tree. To be implemented later
+    public Waypoint FindClosestWaypoint(Transform currentPosition)
+    {
+        // Find the closest distance
+        float distance;
+        float closestDistance = Mathf.Infinity;
+        Waypoint closestWaypoint = null;
+
+        for (int i = 0; i < waypoints.Count; i++)
+        {
+            distance = Mathf.Pow(waypoints[i].transform.position.x - currentPosition.position.x, 2) + Mathf.Pow(waypoints[i].transform.position.y - currentPosition.position.y, 2) + Mathf.Pow(waypoints[i].transform.position.z - currentPosition.position.z, 2);
+            if(distance < closestDistance)
+            {
+                closestDistance = distance;
+                closestWaypoint = waypoints[i];
+            }
+        }
+        return closestWaypoint;
+    }
+
+
+    // Pathfinding. Either function can be called and will return a list containing the path
+    public List<Waypoint> FindPath(Waypoint start, Transform goal)
+    {
+        return FindPath(start, FindClosestWaypoint(goal));
+    }
+    // Making use of A*
+    public List<Waypoint> FindPath(Waypoint start, Waypoint end)
+    {
+        List<Waypoint> closedList = new List<Waypoint>();   // Create a list containing nodes already seen (start is default)
+        List<Waypoint> openList = new List<Waypoint>() { start };   // Create a list containing nodes seen but not assessed yet. Ordered as a priority queue
+
+        List<Waypoint> path = new List<Waypoint>(); // List containing all the nodes in the shortest path
+
+        List<Waypoint> currentLinks = new List<Waypoint>(); // Contains the neighbouring nodes to be assessed from the current node
+        Waypoint currentWaypoint = start;
+
+        // Init of the starting node
+        currentWaypoint.SetHeuristic(Mathf.Pow(end.gameObject.transform.position.x, 2) + Mathf.Pow(end.gameObject.transform.position.y, 2) + Mathf.Pow(end.gameObject.transform.position.z, 2));
+        currentWaypoint.AssignNodeValue(0);
+        currentWaypoint.AssignChild(null);
+
+        bool foundEnd = false;
+
+        while ((openList.Count > 0) && (!foundEnd))
+        {
+            currentWaypoint = RemoveHeap(openList);
+            closedList.Add(currentWaypoint);    // Move the currently assessed waypoint to the closed list and mark it as completed
+            currentLinks = currentWaypoint.GetConnectedNodes(); // Take all the nodes that the current node is connected to
+
+            for(int i = 0; i < currentLinks.Count; i++)
+            {
+                // Make sure it's not already in the closed list. Ignore it otherwise
+                if (!closedList.Contains(currentLinks[i]))
+                {
+                    ////// TEMPORARY TESTING OF OBSTACLES////////////////
+                    if (currentLinks[i].IsBlocked())
+                        closedList.Add(currentLinks[i]);
+                    ////////////////////////////////////////////////////
+                    else
+                    {
+                        // Using Euclidean distance as heuristic function since waypoints are set at diff angles
+                        float heuristic = Mathf.Pow(end.gameObject.transform.position.x - currentLinks[i].gameObject.transform.position.x, 2) + Mathf.Pow(end.gameObject.transform.position.y - currentLinks[i].gameObject.transform.position.y, 2) + Mathf.Pow(end.gameObject.transform.position.z - currentLinks[i].transform.position.z, 2);
+                        float gValue = Mathf.Pow(currentLinks[i].gameObject.transform.position.x - currentWaypoint.gameObject.transform.position.x, 2) + Mathf.Pow(currentLinks[i].gameObject.transform.position.y - currentWaypoint.gameObject.transform.position.y, 2) + Mathf.Pow(currentLinks[i].gameObject.transform.position.z - currentWaypoint.transform.position.z, 2) + currentWaypoint.GetGVal();
+
+                        // If this item has not been seen OR if it has, that it has a higher cost value
+                        if ((!openList.Contains(currentLinks[i]) || (openList.Contains(currentLinks[i]) && (currentLinks[i].GetNodeValue() > gValue + heuristic))))
+                        {
+                            currentLinks[i].AssignChild(currentWaypoint);
+
+                            // Assign the distance to each node
+                            currentLinks[i].SetHeuristic(Mathf.Sqrt(heuristic));
+                            //currentLinks[i].SetHeuristic(0);  // Turn this into Dijkstra's
+                            currentLinks[i].AssignNodeValue(gValue);
+
+                            AddHeap(openList, currentLinks[i]);
+                        }
+                    }
+                }
+                // If the end is found, exit
+                if (currentLinks[i] == end)
+                {
+                    currentWaypoint = end;
+                    i = currentLinks.Count;
+                    foundEnd = true;
+                }
+            }
+        }
+
+        // If a path was found, otherwise the path list will return empty
+        if(foundEnd)
+        {
+            while(currentWaypoint != null)
+            {
+                path.Add(currentWaypoint);
+                currentWaypoint = currentWaypoint.GetChild();
+            }
+            path.Reverse(); // Or can implement as a stack
+        }
+
+        return path;
+    }
+
+    // Create the priority queue
+    void AddHeap(List<Waypoint> heap, Waypoint add)
+    {
+        Waypoint temp = null;
+        heap.Add(add);
+
+        int i = heap.Count - 1;
+
+        while(i > 0)
+        {
+            if (heap[i].GetNodeValue() < heap[i / 2].GetNodeValue())
+            {
+                temp = heap[i];
+                heap[i] = heap[i / 2];
+                heap[i / 2] = temp;
+
+                i = i / 2;
+            }
+            else
+                i = 0;
+        }
+    }
+    Waypoint RemoveHeap(List<Waypoint> heap)
+    {
+        Waypoint nextWaypoint = heap[0];
+        Waypoint temp = null;
+        heap.RemoveAt(0);
+
+        if(heap.Count > 0)
+        {
+            temp = heap[0];
+
+            int i = 0;
+
+            while (i < heap.Count)
+            {
+                if ((i * 2 + 1 < heap.Count) && (heap[i].GetNodeValue() < heap[i * 2 + 1].GetNodeValue()))
+                {
+                    if ((i * 2 + 2 < heap.Count) && (heap[i * 2 + 1].GetNodeValue() < heap[i * 2 + 2].GetNodeValue()))
+                        i = i * 2 + 2;
+                    else
+                        i = i * 2 + 1;
+                    temp = heap[i];
+                    heap[i] = heap[i / 2];
+                    heap[1 / 2] = temp;
+                }
+                else
+                    i = heap.Count;
+            }
+        }
+        return nextWaypoint;
+    }
+}
