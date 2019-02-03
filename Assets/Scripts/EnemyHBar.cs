13a5b552bf2ee301aad331d28f02ff9a25603bd0
cy1mat1auto <46876701+cy1mat1auto@users.noreply.github.com>
2019-02-02 20:26:50 -0500

    EnemyHealthDisplay
    Created a script that draws a slider over an enemy as long as it's within view of the camera. 60% of the time works every time (I'll fix it later; it needs quite a few more behaviors).

---
+++ Assets/Scripts/EnemyHBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHBar : MonoBehaviour
+{
+    public Camera PlayerView;
+    public GameObject Holder;
+    public Slider Bar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Holder.transform.LookAt(PlayerView.transform);
+    }
+}
---
+++ Assets/Scripts/EnemyHBar2.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHBar2 : MonoBehaviour
+{
+    //Attach this script to the enemy, assign the itself to be Enemy, and a UI slider to be Bar.
+
+    public Camera PlayerView;
+    public Canvas HUD;
+    public GameObject OverallBar;
+    public Slider Bar;
+    public GameObject Enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Bar.transform.SetParent(HUD.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Enemy.GetComponentInParent<Renderer>().isVisible)
+        {
+            OverallBar.SetActive(true);
+            Bar.transform.position = PlayerView.WorldToScreenPoint(Enemy.transform.position);
+        }
+        
+        else
+        {
+            OverallBar.SetActive(false);
+        }
+    }
+}
