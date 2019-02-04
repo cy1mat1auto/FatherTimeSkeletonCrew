69f4771872347a846f866d30e2b62023e4ed8fd7
Thomas Connolly <ulycc634@gmail.com>
2019-02-03 20:57:12 -0500

    Pushing lasers.

---
+++ Assets/Scripts/JockeyWeapons.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JockeyWeapons : MonoBehaviour
+{
+
+	private RaycastHit vision;
+	private float rayLength;
+    public static bool onObject;
+    public static Vector3 laserEnd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		rayLength = 200;
+        onObject = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(transform.position, transform.forward * rayLength, Color.red, 8.5f);
+        onObject = Physics.Raycast(transform.position, transform.forward * rayLength, out vision, rayLength);
+        if (onObject)
+        {
+            laserEnd = vision.collider.transform.position;
+        }
+        else
+        {
+            laserEnd = transform.forward * 100000;
+        }
+    }
+}
---
+++ Assets/Scripts/laserScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class laserScript : MonoBehaviour {
+	public Transform startPoint;
+	LineRenderer laserLine;
+    private bool pressed;
+
+	// Use this for initialization
+	void Start () {
+		laserLine = GetComponentInChildren<LineRenderer> ();
+		laserLine.SetWidth (.2f, .2f);
+        pressed = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            pressed = true;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            pressed = false;
+        }
+        if (pressed)
+        {
+            laserLine.SetPosition(0, startPoint.position);
+            laserLine.SetPosition(1, JockeyWeapons.laserEnd);
+        }
+        else if (!pressed)
+        {
+            laserLine.SetPosition(0, startPoint.position);
+            laserLine.SetPosition(1, startPoint.position);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+
+        }
+    }
+}
