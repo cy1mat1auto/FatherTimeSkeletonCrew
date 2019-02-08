bb97f6dc1a351eba7756780b7a2ca9f475f212d1
cy1mat1auto <46876701+cy1mat1auto@users.noreply.github.com>
2019-02-08 18:39:05 -0500

    Preserving Settings and Organizing
    Created a folder within the main Scripts folde to hold level-management scripts, and added a simple script that preserves settings between the MainMenu scene and any selected scene.

--- Assets/Scripts/LvlMgmt/LevelManager.cs
+++ Assets/Scripts/LvlMgmt/LevelManager.cs
@@ -10,6 +10,14 @@ public class LevelManager : MonoBehaviour
     //Create Buttons for each level. Use the inspector to drag-and-drop the buttons into the slots.
     public Button level1, level2, level3, level4, level5;
     public string l1, l2, l3, l4, l5;
+    public GameObject PreservedSettings;
+
+    //Void Awake will protect PreservedSettings; This is a gameobject containing scripts that hold the player's
+    //upgrades, audiovisual and other gameplay settings;
+    //private void Awake()
+    //{
+        //DontDestroyOnLoad(PreservedSettings);
+    //}
 
     //Add a listener to the each button. On click, the listener will then call the SceneManager.
     void Start()
@@ -20,7 +28,8 @@ public class LevelManager : MonoBehaviour
     // create a void function that calls the scenemanager to load the desired scene associated with each button.
     void LoadLevel1()
     {
-        SceneManager.LoadScene(l1);
+        SceneManager.LoadScene(l1, LoadSceneMode.Single);
+        DontDestroyOnLoad(PreservedSettings);
     }
 
 }
---
+++ Assets/Scripts/LvlMgmt/LoadSettings.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSettings : MonoBehaviour
+{
+    //Stores a series of settings that are loaded into the next scene
+    public float maxhealth;
+
+    void Awake()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += SettingAssigner;
+    }
+
+    void SettingAssigner(Scene scene, LoadSceneMode mode)
+    {
+        if(scene.name != "MainMenu")
+        {
+            GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>().MaxHealth = maxhealth;
+        }
+    }
+
+}
