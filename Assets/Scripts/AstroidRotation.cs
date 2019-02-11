a86be07a80bcfd3f5f24cee9fa412677e30cbca3
Thomas Connolly <ulycc634@gmail.com>
2019-02-11 16:03:26 -0500

    Pushing astroid rotations.

---
+++ Assets/Scripts/AstroidRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstroidRotation : MonoBehaviour
+{
+    private Vector3 rot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rot = new Vector3(Random.Range(-1, 1), Random.Range(-1, 1), Random.Range(-1, 1));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rot);
+    }
+}
