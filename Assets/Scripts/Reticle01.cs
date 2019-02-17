090f328e2fa3118e2669120338e84635a7265792
cy1mat1auto <46876701+cy1mat1auto@users.noreply.github.com>
2019-02-16 21:44:51 -0500

    Skybox, UI assets and camera
    Skybox has been rebuilt with square pieces. Quit button now has a UI texture. Angling the player camera down by 4 degrees makes the flying feel much better (implemented in TestBed1 only).

---
+++ Assets/Scripts/Reticle01.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Reticle01 : MonoBehaviour
+{
+    public GameObject target;
+    public Camera PlayerView;
+    public Canvas HUD;
+    public GameObject reticle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        reticle.transform.position = PlayerView.WorldToScreenPoint(target.transform.position);
+    }
+}
