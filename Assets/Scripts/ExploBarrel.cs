44065666bd2536be8727017d529924a69c0b5129
cy1mat1auto <46876701+cy1mat1auto@users.noreply.github.com>
2019-02-17 16:32:22 -0500

    Unfinished Ship
    Started modeling another ship. Explosive barrel now destroys terrain (popping it out of existance). I'll keep working on it.

---
+++ Assets/Scripts/ExploBarrel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExploBarrel : MonoBehaviour
+{
+    public ParticleSystem ExploSource;
+    public float health = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health == 0)
+        {
+            Collider [] InRadius = Physics.OverlapSphere(transform.position, 100f);
+            Debug.Log(InRadius.Length);
+            foreach(Collider hits in InRadius)
+            {
+                if(hits.CompareTag("Terrain"))
+                {
+                    Debug.Log(hits.name);
+                    Destroy(GameObject.Find(hits.name));
+                    Debug.Log("TerrainGone");
+                }
+            }
+        }
+    }
+}
