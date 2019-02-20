8baf27ba89384c6d77688abb6766f2f8260d9a19
Thomas Connolly <ulycc634@gmail.com>
2019-02-20 17:44:10 -0500

    Adding laser charge.

--- Assets/Scripts/laserScript.cs
+++ Assets/Scripts/laserScript.cs
@@ -1,15 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class laserScript : MonoBehaviour {
 	public Transform startPoint;
 	LineRenderer laserLine;
     public bool pressed;
 
+    public Slider laserCharge;
+    public float depleatSpeed = 0.001f, reloadSpeed = 0.0003f;
+
 	// Use this for initialization
 	void Start () {
 		laserLine = GetComponentInChildren<LineRenderer> ();
-		laserLine.SetWidth (.2f, .2f);
+		laserLine.SetWidth (0.2f, 0.2f);
         pressed = false;
 	}
 	
@@ -23,20 +27,23 @@ public class laserScript : MonoBehaviour {
         {
             pressed = false;
         }
+
         if (pressed)
+        {
+            if (laserCharge.value > 0)
+                laserCharge.value -= depleatSpeed;
+        }
+        else
+            laserCharge.value += reloadSpeed;
+        if (pressed && laserCharge.value > 0.05)
         {
             laserLine.SetPosition(0, startPoint.position);
             laserLine.SetPosition(1, JockeyWeapons.laserEnd);
         }
-        else if (!pressed)
+        else if (!pressed || laserCharge.value <= 0.05)
         {
             laserLine.SetPosition(0, startPoint.position);
             laserLine.SetPosition(1, startPoint.position);
-        }
-
-        if (Input.GetMouseButtonDown(1))
-        {
-
         }
     }
 }
