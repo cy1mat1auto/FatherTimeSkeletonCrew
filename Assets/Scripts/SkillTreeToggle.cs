8ad0f8e3b1369a728d568c19d3f0f66727eb8dac
cy1mat1auto <46876701+cy1mat1auto@users.noreply.github.com>
2019-02-23 19:03:52 -0500

    Toggle Locking Script
    The toggle now has a script attached that locks it in the "true" state after the first click.

---
+++ Assets/Scripts/SkillTreeToggle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillTreeToggle : MonoBehaviour
+{
+    //Attach this script to the toggle;
+    public bool purchased = false;
+    private Toggle Skill;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Skill = GetComponent<Toggle>();
+        Skill.isOn = false;
+        Skill.onValueChanged.AddListener(delegate { GetUpgrade(); });
+    }
+
+    // Update is called once per frame
+    void GetUpgrade()
+    {
+        if (!purchased)
+        {
+            Skill.isOn = true;
+            purchased = true;
+        }
+
+        else
+        {
+            Skill.isOn = true;
+        }
+    }
+
+}
