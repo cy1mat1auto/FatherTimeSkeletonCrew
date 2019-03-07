ab5de20b4d8bbf513d109ca6fc63159b856ae8e0
cy1mat1auto <46876701+cy1mat1auto@users.noreply.github.com>
2019-03-06 20:27:53 -0500

    Richard's AI
    Given the performance issues with the current A* pathfinding setup, I have started playing around with a decision-tree style navigation system. Not sure where this will go yet. Carry on.

---
+++ Assets/Scripts/Pathfinding/NPCCirclePath.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCCirclePath : MonoBehaviour {
+
+    //Based on one of Richard's first unity scripts.
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0,0,10f), ForceMode.Force);
+        GetComponent<Rigidbody>().rotation *= Quaternion.Euler(new Vector3(0, 0.2f, 0));
+        //transform.Rotate(new Vector3(0, 0.5f, 0), Space.World);
+    }
+}
