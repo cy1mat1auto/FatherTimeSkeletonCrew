e1f132726114f9a94fcd3e1c43d67324d31f8875
Thomas Connolly <ulycc634@gmail.com>
2019-03-27 17:53:05 -0400

    Expanded level1 and added small extra features.

--- Assets/Scripts/ExploBarrel2.cs
+++ Assets/Scripts/ExploBarrel2.cs
@@ -6,28 +6,41 @@ public class ExploBarrel2 : MonoBehaviour
 {
     public ParticleSystem explosion;
     public float health, damage;
+    private int timer;
 
     // Start is called before the first frame update
     void Start()
     {
         explosion.Pause();
+        timer = 100;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //print(health);
         if (health == 0)
         {
             explosion.transform.position = transform.position;
             explosion.Play();
             transform.GetComponent<Renderer>().enabled = false;
-            Collider[] inRadius = Physics.OverlapSphere(transform.position, 100f);
+
+            Collider[] inRadius = Physics.OverlapSphere(transform.position, 50f);
             foreach (Collider hits in inRadius)
             {
                 if (hits.CompareTag("Terrain"))
                     Destroy(GameObject.Find(hits.name));
                 else if (hits.CompareTag("Enemy"))
+                    hits.gameObject.GetComponent<EnemyHealth>().CurrentHealth -= damage;
+                else if (hits.CompareTag("Player"))
+                    hits.gameObject.GetComponent<PlayerHealth>().CurrentHealth -= damage;
+                else if (hits.CompareTag("Explosive"))
+                    hits.GetComponent<ExploBarrel2>().health -= 1;
+            }
+
+            Collider[] inRadius2 = Physics.OverlapSphere(transform.position, 20f);
+            foreach (Collider hits in inRadius)
+            {
+                if (hits.CompareTag("Enemy"))
                     hits.gameObject.GetComponent<EnemyHealth>().CurrentHealth -= damage;
                 else if (hits.CompareTag("Player"))
                     hits.gameObject.GetComponent<PlayerHealth>().CurrentHealth -= damage;
