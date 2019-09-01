3ce8a07c713e300f5834087c4f29063940e1bdaf
cy1mat1auto <46876701+cy1mat1auto@users.noreply.github.com>
2019-09-01 09:51:27 -0400

    HD Update 0.2
    Animations have been added. Some transitions still need to be added.

---
+++ Assets/Scripts/PlayerAnim.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnim : MonoBehaviour
+{
+    public Animator Animator;
+    private int Speed, Strafe;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Animator == null)
+        {
+            Animator = GetComponent<Animator>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Input.anyKey)
+        {
+            Speed = 0;
+            Strafe = 0;
+        }
+
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift))
+        {
+            Speed = 2;
+        }
+
+        else if (Input.GetKey(KeyCode.W))
+        {
+            Speed = 1;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Strafe = 1;
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Strafe = 2;
+        }
+
+        else
+        {
+            Strafe = 0;
+        }
+
+        Animator.SetInteger("Speed", Speed);
+        Animator.SetInteger("Strafe", Strafe);
+    }
+}
