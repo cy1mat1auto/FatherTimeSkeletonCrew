7ffbe5e229d0fefef0d90bc613a9ca59c51c8bd5
cy1mat1auto <46876701+cy1mat1auto@users.noreply.github.com>
2019-10-30 20:39:06 -0400

    The Killable Enemies Update
    EnemyHealth script now handles cancelling raycasts, disabling AI and destroying the enemy once health reaches zero.

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -8,6 +8,9 @@ public class EnemyHealth : MonoBehaviour
     public Collider Crit1;
     public float MaxHealth;
     public float CurrentHealth;
+    public float DespawnDelay = 1.5f;
+    private float DeathTime;
+    private bool Dead = false;
 
 
     // Start is called before the first frame update
@@ -20,5 +23,25 @@ public class EnemyHealth : MonoBehaviour
     void Update()
     {
         CurrentHealth = Mathf.Clamp(CurrentHealth, 0, MaxHealth);
+
+        if (CurrentHealth <= 0f)
+        {
+            if (!Dead)
+            {
+                DeathTime = Time.time;
+                Destroy(GetComponent<EnemyHBar2>().OverallBar);
+                GetComponent<EnemyAI_RL3>().SendMessage("EndBehavior");
+                GetComponent<EnemyAI_RL3>().enabled = false;
+                Dead = true;
+            }
+
+            else
+            {
+                if (Time.time - DeathTime >= DespawnDelay)
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
     }
 }
